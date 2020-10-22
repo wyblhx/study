b0fcc7431e1467c436491e8e3a4c4a15b7a1a8d7
wyblhx <2248720753@qq.com>
2020-10-22 12:07:44 +0800

    a simple program

--- python/shopping.py
+++ python/shopping.py
@@ -1,31 +1,26 @@
-# 购物系统
-
-
-is_sign_in = False
-users = []  # 用户组
-
+# 一个简单的购物系统，包括用户注册、用户登陆、添加商品到购物车中
 
 # sign up 用户注册
 def sign_up():
     while True:
         user = {}  # 用户
-        is_resignin = False
+        is_registered = False  # 判断用户是否要继续注册，默认不继续注册
         username = input('请输入用户名：')
         password = input('请输入密码：')
         repassword = input('请再次输入密码：')
         if password == repassword:
-            # 将用户加入到用户组中
+            # 1.检查用户组用户名是否存在重复
             user['username'] = username
             user['password'] = password
-            # 检查用户组用户名是否存在重复
             for user_ in users:
                 if username == user_['username']:
                     print('用户名重复，请重新输入！')
-                    is_resignin = True
+                    is_registered = True
                     break
-            if is_resignin:
-                is_resignin = False
+            if is_registered:
+                is_registered = False
                 continue
+            # 2.将用户加入到用户组中
             users.append(user)
             print('用户注册成功')
             break
@@ -38,16 +33,39 @@ def sign_up():
                 break
 
 
-# 用户登录
-def sign_in(username, password):
+# 用户登陆操作
+def log_in():
+    # 1. 用户账号和密码
+    username = input('请输入用户名：')
+    password = input('请输入密码：')
     # 判断用户名和密码是否正确
     for user in users:
         if username == user['username'] and password == user['password']:
             return True
 
 
+# 购物
+def add_shoppingcart(goodsName):
+    global is_log_in
+    while True:
+        if is_log_in:
+            # 已经登陆
+            if goodsName:
+                print('用户成功将{}加入到购物车中'.format(goodsName))
+                break
+        else:
+            # 用户没有进行登陆
+            answer = input('用户没有进行登陆，是否要登陆？（yes/no）')
+            if 'y' in answer:
+                is_log_in = log_in()
+            else:
+                print('很遗憾，不能添加商品到购物车中')
+
+
 if __name__ == '__main__':
-    is_newuser = True
+    is_log_in = False  # 判断用户是否登陆，默认没有登陆
+    users = []  # 用户组
+    is_newuser = True  # 判断用户是否为新用户,default=True
     # 进行至少一次注册操作,可以进行多次注册
     while True:
         if is_newuser:
@@ -58,9 +76,6 @@ if __name__ == '__main__':
             sign_up()
         else:
             break
-    # 进行登录操作
-    username_ = input('请输入用户名：')
-    password_ = input('请输入密码：')
-    is_sign_in = sign_in(username_, password_)
-    if is_sign_in:
-        print('用户登录成功')
+    # 购物
+    goodsName = input('请输入你想买的商品：')
+    add_shoppingcart(goodsName)
